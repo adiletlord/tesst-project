fff5238d0f171ff4046df81d9b4836e8eec9c173
adiletlord <sverhrikudo@gmail.com>
2023-03-31 11:19:02 +0300

    comit

---
+++ src/redux/reducer.jsx
@@ -0,0 +1,26 @@
+const defaultState = {
+    countries: [],
+    item: [],
+    errorMessage: null
+}
+
+export const reducer = (state = defaultState, action) => {
+    const { payload, type, errorMessage } = action;
+    switch (type) {
+        case "LOAD_ITEM": {
+            return {
+                ...state,
+                item: payload,
+                errorMessage: errorMessage
+            }
+        }
+        case "LOAD_ALL_COUNTRY": {
+            return {
+                ...state,
+                countries: payload
+            }
+        }
+        default:
+            return state
+    }
+}
